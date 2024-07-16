b52a5c3277b858b0128dee1a7f9ed6aa9f035759
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-07-17 00:55:49 +0530

    add notes func

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -12,18 +12,46 @@ const Notes = () => {
       content: "This is another note",
     },
   ]);
+  const [content, setContent] = useState("");
+  const [error, setError] = useState("");
+
+  const createNewNote = (newNoteContent) => {
+    if (content === "") {
+      setError("Note cannot be empty");
+      return;
+    }
+    setNotes([
+      ...notes,
+      {
+        id: notes.length + 1,
+        content: content,
+      },
+    ]);
+    setContent("");
+    setError("");
+  };
 
   return (
     <main className="mt-10">
-      <div className="flex justify-center gap-1">
-        <input
-          type="text"
-          placeholder="Create a new note"
-          className="border border-black rounded-md p-2"
-        />
-        <button className="bg-red-500 p-2 rounded-md border border-black">
-          Create
-        </button>
+      <div className="flex flex-col justify-center ">
+        <div className="flex justify-center gap-1">
+          <input
+            type="text"
+            placeholder="Create a new note"
+            className="border border-black outline-red-600 rounded-md p-2"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <button
+            onClick={createNewNote}
+            className="bg-red-500 p-2 rounded-md border border-red-600"
+          >
+            Create
+          </button>
+        </div>
+        <div className="text-center">
+          {error && <p className="text-red-500">{error}</p>}
+        </div>
       </div>
       <div>
         {notes.map((note) => {
